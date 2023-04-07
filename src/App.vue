5da73b4113f62df070e1d0167bd4fe3ef45287e4
Daniils Firgers <dfirger@gmail.com>
2023-04-07 12:45:51 +0300

    add vue router to the project

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    <div class="flex bg-gray-100">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click me</button>
-    </div>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<RouterView></RouterView>
 </template>
 
 <style scoped>
