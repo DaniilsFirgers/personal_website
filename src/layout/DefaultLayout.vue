b2bed68f724981e265a39f74a606267429ec724b
Daniils Firgers <dfirger@gmail.com>
2023-04-09 21:43:37 +0300

    fix settings modal

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { ref, onBeforeUnmount } from 'vue'
 import moment from 'moment-timezone'
-import { vOnClickOutside } from '@vueuse/components'
+import { OnClickOutside } from '@vueuse/components'
 
 const beforeShutDownSecLimit: number = 10
 const brightnessValue = ref<string>("100")
@@ -16,12 +16,8 @@ const intervalCount = ref(0);
 const intervalId = ref<number|null>(null);
 function startSwitchOffInterval(){
     if (!intervalId.value) {
-            
-       
-    intervalId.value = setInterval(()=>{
-        
+    intervalId.value = setInterval(()=>{   
         intervalCount.value++;
-        console.log(intervalCount.value)
         remainingTimeBeforePowerOff.value--
 
         if(intervalCount.value === 10 && intervalId.value){
@@ -38,7 +34,6 @@ function currentDatetime() {
 
     const updateCurrentDatetime = ()=> {
         currentTime.value = moment().format(timeFormat)
-        console.log(currentTime)
     }
     const updateDatetimeInterval = setInterval(updateCurrentDatetime, 1000)
     onBeforeUnmount(()=>{
@@ -54,10 +49,10 @@ currentDatetime()
 
 function onHandleHeaderModal(){
     openHeaderSettingsModal.value = !openHeaderSettingsModal.value
-
 }
-
-
+function onHandleOutsideModal(){
+    openHeaderSettingsModal.value = false
+}
 
 function onHandlePowerOff(){
     powerOff.value = true
@@ -86,7 +81,7 @@ function closeSwitchOffModal(){
     <v-container class="py-0 px-0 flex justify-start"><h1 class="pl-4">Activities</h1> </v-container>
     <v-container class="py-0 px-0 flex justify-center"><h1 class="text-white">{{ currentTime.toLocaleLowerCase() }}</h1></v-container>
     <v-container class="py-0 px-0 flex justify-end">
-        <button @click="onHandleHeaderModal">Open</button>
+        <button @click="onHandleHeaderModal" class="open-settings-modal">Open</button>
     </v-container>
 </header>
 <div class="flex flex-row">
@@ -96,20 +91,25 @@ function closeSwitchOffModal(){
     </div>
     
 
-   </div>
+   </div> 
        <div class="h-[calc(100vh_-_1.5rem)] bg-mid-aubergine w-full relative" :class="{'brightness-0': powerOff}">
-        <div class="h-80 w-72 bg-header-pop-up rounded-lg absolute top-1.5 right-1.5" v-if="openHeaderSettingsModal" v-on-click-outside="onHandleHeaderModal">
+        
+        <OnClickOutside @trigger="onHandleOutsideModal" :options="{ignore: ['.open-settings-modal']}" v-if="openHeaderSettingsModal">
+        <div class="h-80 w-72 bg-header-pop-up rounded-lg absolute top-1.5 right-1.5"  >
         
         <div @click="openSwitchOffModal"> <span class="flex items-center cursor-pointer"> <v-icon
           end
           icon="mdi-power"
         ></v-icon>Power Off/Logout</span>  </div>    
+       
         
-  
         </div>
+    </OnClickOutside>
+
+
         <div v-if="showSwitchOffModal" class="h-36 w-2/6 bg-header-pop-up rounded-t-lg absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <section class="flex flex-col justify-center align-center items-center">
-                <h1 class="mt-5">Power Off</h1>
+                <h1 class="mt-5 text-lg">Power Off</h1>
                 <h2 class="mt-5 mx-auto">The system will power off automatically in {{ remainingTimeBeforePowerOff }} seconds.</h2>
             </section>
             <section class="mt-5">
