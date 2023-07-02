6afdb355cff390c63c28af654292370b45c89302
Daniils Firgers <dfirger@gmail.com>
2023-07-02 21:00:25 +0300

    add vite hmr to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
+  server: {
     host: true,
-    port: 3003
+    port: 3003,
+    hmr: {
+      path: "/__vite_hmr",
+    },
   },
   // build:{
   //   rollupOptions: {
@@ -19,4 +22,4 @@ export default defineConfig({
   //     }
   // }
   // }
-})
+});
